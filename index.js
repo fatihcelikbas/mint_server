70ccb43d69c39f78119325111d532ebb72e6fe01
Fatih Celikbas <clkbs.fth@gmail.com>
2021-11-22 17:10:49 -0500

    All the rest

--- index.js
+++ index.js
@@ -1,44 +1,108 @@
+require("dotenv").config();
+
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const helmet = require('helmet');
+const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const mongoose = require("mongoose");
+const Knight = require('./models/Knight');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); 
+const db = mongoose.connection;
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connection to db established"));
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-bepfwwd0.us.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://server.fatihcelikbas.com',
+  issuer: `https://dev-bepfwwd0.us.auth0.com/`,
+  algorithms: ['RS256']
+});
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
-app.use(cors())
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send("Welcome to your App!")
-})
+app.use(cors());
 
-app.get('/users', (req, res) => {
-  axios.get("https://jsonplaceholder.typicode.com/users")
-  .then(response => {
-    res.json(response.data)
-  })
-  .catch(error => {
-    res.json("Error occured!")
-  })
-})
+app.use(morgan('combined'));
 
-app.post('/getUserById', (req, res) => {
-  if (!req.body.id) {
-    res.json("No ID found in reqest body.")
-  }
-  else {
-    axios.get(`https://jsonplaceholder.typicode.com/users/${req.body.id}`)
-    .then(function(response) {
-        res.json(response.data)
-    }).catch(function(error) {
-        res.json("Error occured!")
-    }) 
+app.get('/', async (req, res) => {
+  try {
+    const knights = await Knight.find()
+    res.json(knights)
+  } catch (err) {
+    res.status(500).json({message: err.message})
   }
 })
+
+app.post('/mint', async (req, res) => {
+  let knight;
+  try {
+    knight = await Knight.findOne({isMinted: false});
+    if (knight == null) {
+      return res.status(404).json({ message: "All minted" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  knight.isMinted = true;
+  knight.save(function (err) {
+    if(err) {
+        return res.status(500).json({ message: err.message });
+    }
+  });
+
+  res.json({ minted: knight.cid});
+})
+
+app.get('/:id', async (req, res) => {
+  let knight;
+  try {
+    knight = await Knight.findOne({cid: req.params.id.toString()});
+    if (knight == null || !knight.isMinted) {
+      return res.status(404).json({ message: "Cannot find Knight" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.json(knight);
+});
+
+app.use(checkJwt);
+
+app.post('/add', (req, res) => {
+  const knight = new Knight({ 
+    name: req.body.name, 
+    description: req.body.description,
+    image: req.body.image, 
+    attributes: req.body.attributes, 
+    cid: req.body.id});
+
+  knight.save(function (err) {
+    if(err) {
+        return res.status(500).json({ message: err.message });
+    }
+  });
+
+  res.json({added: req.body.id});
+});
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
-})
+});
