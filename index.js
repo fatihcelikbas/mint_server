552fdc79d8154f68655179fd0e24cbac5a477c37
Fatih Celikbas <clkbs.fth@gmail.com>
2021-11-22 12:52:58 -0500

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000
 const app = express();
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
